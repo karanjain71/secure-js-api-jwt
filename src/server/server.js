0faa4766a3bf94622a239aede4c35a23603cddaa
Karan Jain <karan.jain@sap.com>
2022-01-22 19:05:18 +0530

    Added the changes

--- src/server/server.js
+++ src/server/server.js
@@ -4,20 +4,42 @@ const {
   getUserByUsername,
   isEmptyObject,
   isPasswordCorrect,
+  getAllBooks,
+  getAllUsers,
+  addBook
 } = require("./shared");
+const { uuid } = require("uuidv4");
 const app = express();
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(express.json());
 app.use(cors());
 
-app.get("/users", (req, res) => {});
+app.get("/users", (req, res) => {
+  console.log('Inside the user api call')
+  getAllUsers().then(users => {
+    console.log(users, 'We are inside the users')
+    if(users && users.length>0) res.status(200).send({users:users})
+    else res.status(500).send({users:[]})
+  })
+});
 
-app.get("/books", (req, res) => {});
+app.get("/books", (req, res) => {
+  console.log('Why is this happending in /books')
+  getAllBooks().then(books => {
+    if(books && books.length>0){
+      res.status(200).send({books:books})
+    }
+    else{
+      res.status(500).send({books:[]})
+    }
+  })
+});
 
 app.post("/login", (req, res) => {
   let base64Encoding = req.headers.authorization.split(" ")[1];
   let credentials = Buffer.from(base64Encoding, "base64").toString().split(":");
+  console.log(credentials,'These are the credentials')
   const username = credentials[0];
   const password = credentials[1];
   getUserByUsername(username).then((user) => {
@@ -38,4 +60,10 @@ app.get("/logout", (req, res) => {});
 
 app.get("/favorite", (req, res) => {});
 
-app.post("/book", (req, res) => {});
+app.post("/book", (req, res) => {
+  addBook({name:req.body.name, author:req.body.author,id:uuid()}).then(
+    err => {
+    if(err) res.status(500).send({message:'Book not added'})
+    else res.status(200).send({message:"Book added successfully"})
+    })
+});
--- src/server/shared.js
+++ src/server/shared.js
@@ -1,4 +1,5 @@
 const jsonfile = require("jsonfile");
+const inventory = "./database/books.json"
 const users = "./database/users.json";
 const bcrypt = require("bcrypt");
 
@@ -19,3 +20,44 @@ exports.isEmptyObject = (object) => Object.entries(object).length === 0;
 exports.isPasswordCorrect = async function (key, password) {
   return bcrypt.compare(password, key).then((result) => result);
 };
+
+exports.getAllBooks = async function() {
+  try{
+    const allBooks = await jsonfile.readFile(inventory);
+    console.log(allBooks,'Inside the inventory')
+    return allBooks
+  }
+  catch(err){
+    console.log('Error getting books', err)
+  }
+}
+
+exports.getAllUsers = async function(){
+  try{
+    const allUsers = await jsonfile.readFile(users)
+    let updatedUsers = []
+    allUsers.forEach(user => {
+      updatedUsers.push({
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role
+      })
+    })
+    return allUsers
+  }
+  catch(err){
+    console.log('Error getting users', err)
+  }
+}
+
+exports.addBook = async function(book) {
+  try{
+    const allBooks = await jsonfile.readFile(inventory)
+    allBooks.push(book)
+    return await jsonfile.writeFile(inventory)
+  }
+  catch(err){
+    return err
+  }
+}
